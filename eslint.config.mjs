4e7a47f89d2bdac27b383db252e5a1d27756acf1
dipujafar <dipujafarmr@gmail.com>
2024-05-21 20:24:42 +0600

    Start Assignment And Project setUp Done

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    languageOptions: { globals: globals.browser },
+    ignores: ['**/node_modules/', '.dist/'],
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-undef': 'error',
+      'no-console': 'warn',
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
